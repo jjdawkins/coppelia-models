d3f2c1da84f757b22ed4038c1000f0b3aa51708a
allan <allan.elsberry@gmail.com>
2024-05-27 15:58:54 -0400

    made controller function

--- nsaid_module/eval_mat.py
+++ nsaid_module/eval_mat.py
@@ -7,15 +7,22 @@ def create_Wz_C(self):
     l = self.l  # 0.17 for coppeliasim rover
 
     # make theta variables
-    m, j_z, k, c_rr, c_af, c_s, c_d = sp.symbols("m j_z k c_rr c_af c_s c_d", real=True)
+    m, j_z, k, c_rr, c_af, c_s, c_d = sp.symbols(
+        r"m j_z k c_rr c_af c_s c_d", real=True
+    )
+
     # define measured state values
-    x_dot, y_dot, psi_dot = sp.symbols("x_dot y_dot psi_dot", real=True)
+    x_dot, y_dot, psi_dot = sp.symbols("x_dot psi_dot y_dot", real=True)
     x_ddot, y_ddot, psi_ddot = sp.symbols("x_ddot y_ddot psi_ddot", real=True)
     # define desired values
     x_dot_d, y_dot_d, psi_dot_d = sp.symbols("x_dot_d y_dot_d psi_dot_d", real=True)
     x_ddot_d, y_ddot_d, psi_ddot_d = sp.symbols(
         "x_ddot_d y_ddot_d psi_ddot_d", real=True
     )
+
+    k1, k2 = sp.symbols("k_1 k_2", real=True)
+    k_vec = sp.Matrix([k1, k2])
+    k_diag = sp.diag(k1, k2)
 
     # inputs
     i_in, delta_in = sp.symbols("i_in delta_in")
@@ -60,6 +67,40 @@ def create_Wz_C(self):
 
     G = G_u @ u
     sp.pprint(G)
+
+    F = D_m - G
+    F.simplify()
+
+    # make sure that D_m = F + Gu
+    check1 = D_m - (F + G)
+    assert check1.simplify() == None
+
+    # get B and A st G = B * A * u
+    B = sp.Matrix([[1, 0], [0, l], [0, 1]])
+    B_bar = B[:2, :]
+    A = sp.diag(k, i_in)
+
+    # check that G = B * A * u
+    check2 = G - B @ A @ u
+    assert check2.simplify() == None
+
+    W_m = (M @ z_dot).jacobian(theta)
+    W_f = F.jacobian(theta)
+    W_g = G.jacobian(theta)
+
+    # define controller regressor
+    W_m_bar = W_m[:2, :]
+    W_f_bar = W_f[:2, :]
+    W_c = W_m_bar - W_f_bar
+
+    # define the controller
+    delta_z_dot = z_dot - z_dot_d
+    delta_z_dot_bar = delta_z_dot[:2, :]
+    C = sp.Inverse(A) @ B_bar @ W_c @ theta - k_diag @ delta_z_dot_bar
+    C.simplify()
+    sp.pprint(C)
+
+    C_func = sp.lambdify([z_dot_d, z_d, z_dot, theta, k_vec], C, "numpy")
 
 
 if __name__ == "__main__":
