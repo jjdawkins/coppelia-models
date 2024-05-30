5f362d91715fccf22b730b67012e1d1cda0019b4
allan <allan.elsberry@gmail.com>
2024-05-30 11:58:54 -0400

    removed plotting from NSAID

--- nsaid_est.py
+++ nsaid_est.py
@@ -11,28 +11,12 @@ def main(args=None):
     # Create an instance of the parameterEstimation class
     nsaid_est = nsaidEstimation()
 
-    doPlot = 1
-    # turn on interactive plotting
-    plt.ion()
-
     t0 = time.time()
-    # for plotting
     i = 0
-    plot_num = 0
-    plot_funcs = [nsaid_est.plot_vel, nsaid_est.plot_error, nsaid_est.plot_params]
 
     while rclpy.ok():
         i += 1
-
-        # update history data for plotting
-        nsaid_est.update_histories()
-
-        # Update the plots
-        if doPlot and i % 500 == 0:
-            plot_funcs[plot_num]()
-            plot_num = (plot_num + 1) % len(plot_funcs)
-            plt.pause(1e-10)
-        elif not doPlot and i % 10 == 0:
+        if i % 10 == 0:
             # print the parameter estimates
             for i in range(nsaid_est.p):
                 print(f"{nsaid_est.theta_h[i]:.2f}", end=" ")
--- nsaid_module/__init__.py
+++ nsaid_module/__init__.py
@@ -11,14 +11,6 @@ import matplotlib.pyplot as plt
 
 class nsaidEstimation(Node):
     # import methods from other files
-    from ._plot_vel import (
-        plot_vel,
-        update_histories,
-        init_vel_plot,
-        init_error_plot,
-        plot_error,
-    )
-    from ._plot_params import init_param_plot, plot_params
     from ._ref_signals import update_t, get_z_d, update_z_d, init_ref_signals
     from ._run_loop import run_loop
     from ._sub_pub import odom_callback, send_cmd_vel
@@ -30,10 +22,8 @@ class nsaidEstimation(Node):
         print("nsaid_estimation node started")
 
         # call init functions
-        self.init_vel_plot()
-        self.init_error_plot()
+
         self.init_gains()
-        self.init_param_plot()
         self.update_t(setInit=True)
         self.init_ref_signals()
         self.C_func, self.W_z_func = self.create_C_Wz()
@@ -58,11 +48,6 @@ class nsaidEstimation(Node):
         # get starting values
         self.z_dot = np.zeros(3)
         self.update_z_d()
-
-        # init the z_dot history vector
-        self.z_dot_hist = np.zeros((3, 1))
-        self.z_dot_d_hist = np.zeros((3, 1))
-        self.t_hist = np.zeros((1, 1))
 
         # start moving!
         self.send_cmd_vel(2.0, 0.0)
