46c73c3dc9d741b719eaab63c9e0bbf1b241d493
allan <allan.elsberry@gmail.com>
2024-06-20 12:21:56 -0400

    input plot with circle

--- nsaid_module/_ref_signals.py
+++ nsaid_module/_ref_signals.py
@@ -1,23 +1,37 @@
 import numpy as np
-from sympy import symbols, sin, lambdify, Array, diff
+from sympy import symbols, sin, lambdify, Array, diff, atan2
 
 
 def init_ref_signals(self):
     # create the reference functions (use subs to evaluate)
-    t = symbols("t")
-    ref_v = Array(
-        [
-            2.7 + 0.2 * sin(1.41 * t + 2) + 0.5 * sin(0.5 * t),
-            0.3 * sin(0.05 * t + 1) + 0.1 * sin(1.51 * t) + 0.15 * sin(0.31 * t),
-            0,
-        ]
-    )
-    self.ref_v = lambdify(t, ref_v, "numpy")
-    self.ref_v_dot = lambdify(t, diff(ref_v, t), "numpy")
+    # t = symbols("t")
+    # ref_v = Array(
+    #     [
+    #         2.7 + 0.2 * sin(1.41 * t + 2) + 0.5 * sin(0.5 * t),
+    #         0.3 * sin(0.05 * t + 1) + 0.1 * sin(1.51 * t) + 0.15 * sin(0.31 * t),
+    #         0,
+    #     ]
+    # )
+    # self.ref_v = lambdify(t, ref_v, "numpy")
+    # self.ref_v_dot = lambdify(t, diff(ref_v, t), "numpy")
 
-    # make CONSTANT reference values
-    self.ref_v = lambdify(t, Array([2.5, 0.3, 0]), "numpy")
-    self.ref_v_dot = lambdify(t, Array([0, 0, 0]), "numpy")
+    # # make CONSTANT reference values
+    # self.ref_v = lambdify(t, Array([2.5, 0.3, 0]), "numpy")
+    # self.ref_v_dot = lambdify(t, Array([0, 0, 0]), "numpy")
+
+    # make figure 8 reference values
+    t = symbols("t")
+    speed = 0.4
+    x = 1.2 * sin(speed * 2 * t)
+    y = 4 * sin(speed * t)
+    dot_x = diff(x, t)
+    dot_y = diff(y, t)
+
+    theta = atan2(dot_y, dot_x)
+    theta_dot = diff(theta, t)
+
+    self.ref_v = lambdify(t, Array([2, theta_dot]), "numpy")
+    self.ref_v_dot = lambdify(t, Array([0, diff(theta_dot, t)]), "numpy")
 
 
 def update_t(self, setInit=False):
