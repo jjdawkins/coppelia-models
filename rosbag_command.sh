30ac6d05087c5a29b5cdcf54bc3b9fdbbb19e623
allan <allan.elsberry@gmail.com>
2024-08-14 09:57:36 -0400

    updated rosbag command

--- rosbag_command.sh
+++ rosbag_command.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Set the output file name and path
-description="run_0_current"
-output_file=f"rosbags/$description.bag"
 
 # Start recording the /rover/cmd_vel topic
-rosbag record -O "$output_file" /rover/cmd_vel /rover/act_vel /rover/ref_vel /rover/est_param
+ros2 bag record /rover/cmd_vel /rover/act_vel /rover/ref_vel /rover/est_param
